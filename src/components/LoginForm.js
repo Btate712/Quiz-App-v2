18937ac11fbf34d99d187faf52cc5bee2940a923
btate712 <btate712@gmail.com>
2020-04-03 13:49:57 -0500

    login works, need to add styling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useInputHooks } from '../helperFunctions';
 
 function LoginForm(props) {
+
+  const[input, handleInputChange, setInput] = useInputHooks({
+    username: "",
+    password: ""
+  })
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.login(input.username, input.password);
+    setInput({
+      username: "",
+      password: ""
+    });
+  }
+
   return(
-    <>
-      Hello World! Time to Login...
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 col-md-3 col-sm-1"></div>
+        <div className="col-lg-4 col-md-6 col-sm-10">
+          <div className="container border">
+            <h1>User Login</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Username:</label>
+                <input name="username" id="username" type="text" className="form-control" onChange={handleInputChange} value={input.username} />
+              </div>
+              <div className="form-group">
+                <label>Password:</label>
+                <input name="password" id="password" type="password" className="form-control" onChange={handleInputChange} value={input.password} />
+              </div>
+              <input type="submit" className="btn btn-primary"/>
+              <Link to="/register"><button className="btn btn-primary pull-right">Register New User</button></Link>
+            </form>
+            <br />
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-3 col-sm-1"></div>
+      </div>
+    </div>
   )
 }
 
